29a459dd37815ce91b80edde1c9279a22b59f861
JX_Tee <jinxintee@gmail.com>
2025-09-23 12:52:52 +0800

    Refactor: remove useless setting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true,
     host: true,
     port: 5173,
     proxy: {
@@ -14,8 +13,5 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
-  },
-  define: {
-    __API_URL__: JSON.stringify(process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://backend:3000')
   }
 });
